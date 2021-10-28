c835db0f4783f48938ebaa7dfe59d33b9a352e62
nickdimeglio <contact@nickdimeglio.com>
2021-10-28 17:24:41 -0400

    testing input

--- program3/cmd.c
+++ program3/cmd.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 /* smallsh command struct with accompanying functions */
 struct cmd;
@@ -20,7 +21,19 @@ void cmdPrint(struct cmd *cmd) {
 
 /* parse a provided string into a cmd */
 struct cmd *parseCommand(char* cmdString) {
+    
+    // Don't parse if the command is a comment 
+    if (*cmdString == '#') {
+        return NULL;
+    }
+
+    // Don't parse if the command is a blank line
+    bool isBlank = true;
+
+    // Initalize a cmd struct
     struct cmd *cmd = malloc(sizeof(struct cmd)); 
+
+    // Store the command text 
     cmd->text = calloc(strlen(cmdString) + 1, sizeof(char));
     strcpy(cmd->text, cmdString);
 
