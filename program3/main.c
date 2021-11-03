e88c2945d71fcc89279388384079568580937a26
nickdimeglio <contact@nickdimeglio.com>
2021-11-03 18:58:45 -0400

    background processes working (but zombies)

--- program3/main.c
+++ program3/main.c
@@ -8,7 +8,6 @@
 *   Process the file provided as an argument to the program to
 *   create a linked list of movie structs and print out the list.
 *   Compile the program as follows:
-*       gcc --std=gnu99 -o movies main.c
 */
 
 int main(int argc, char *argv[]) {
